cd8f70cde7ea7e929ac93d07f7ee0826d3ac6c87
jose_martinez4 <martinezcaceresjosem@gmail.com>
2023-10-28 21:43:20 -0400

    fix/cors

--- src/index.js
+++ src/index.js
@@ -10,20 +10,19 @@ const port = 3000;
 
 app.use(json());
 
+app.use(urlencoded({ extended: false }));
+app.use(_json());
+
 const allowedOrigins = [
   "http://localhost:3001",
   "http://localhost:3000",
   "https://jmdevv2.vercel.app",
   "https://jmdevv2.vercel.app/contact",
-  "https://send-email-api-josemucv2.vercel.app",
-  "https://send-email-api-ten.vercel.app",
-  "https://send-email-api-omega.vercel.app",
   "https://jmdevv2-josemucv2.vercel.app",
-  "https://send-email-api-flame.vercel.app/api",
-  "https://send-email-api-flame.vercel.app",
   "https://josemartinezcode.com",
   "https://josemartinezcode.com/contact",
   "https://send-email-api-v2.vercel.app",
+  "https://send-email-api-v2-josemucv2.vercel.app",
 ];
 
 const corsOptions = {
@@ -32,14 +31,13 @@ const corsOptions = {
     if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
+      console.log("entra por aca");
       callback(new Error("Acceso no permitido por CORS"));
     }
   },
   methods: "POST",
 };
 
-app.use(urlencoded({ extended: false }));
-app.use(_json());
 app.use(cors(corsOptions));
 app.use("/api", router);
 
